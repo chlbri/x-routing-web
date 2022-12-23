806f3a267d8218a4cb2f3d71ec8b90b470aff543
chlbri <51156116+chlbri@users.noreply.github.com>
2022-12-23 15:36:54 +0000

    Initial commit

---
+++ rollup.config.mjs
@@ -0,0 +1,33 @@
+import tsConfigPaths from 'rollup-plugin-tsconfig-paths';
+import typescript from 'rollup-plugin-typescript2';
+
+/** @type {import('rollup').RollupOptions} */
+export default {
+  input: 'src/index.ts',
+  plugins: [typescript(), tsConfigPaths()],
+  external: [
+    /* TODO: Add external libraries */
+  ],
+  output: [
+    {
+      format: 'es',
+      file: 'lib/index.d.ts',
+    },
+    {
+      format: 'cjs',
+      sourcemap: true,
+      dir: `lib`,
+      preserveModulesRoot: 'src',
+      preserveModules: true,
+      entryFileNames: '[name].js',
+    },
+    {
+      format: 'es',
+      sourcemap: true,
+      dir: `lib`,
+      preserveModulesRoot: 'src',
+      preserveModules: true,
+      entryFileNames: '[name].mjs',
+    },
+  ],
+};
